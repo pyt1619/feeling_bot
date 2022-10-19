897a5dec5054376bbeea974c38c46a019f934885
pyt1619 <54702594+pyt1619@users.noreply.github.com>
2022-10-19 14:40:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '5480060997:AAH33m_plic9ZQP7DLBvl2t3E-J4b2uvhNg' 
-TOKEN2 = '5750848727:AAGKQDbYTgsuQg8R1qt4rNWnnF68Xe5_F6s' 
-ADMINS = [376530980]
+TOKEN = '' # токен основного бота
+TOKEN2 = '' # токен бота админа
+ADMINS = [] # список telegram_id админов
